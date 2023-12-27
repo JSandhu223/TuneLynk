336d0fe7dd4bf73cd9eb9932523b6824765e1823
JSandhu223 <jasrajsandhu01@gmail.com>
2023-12-26 21:58:16 -0700

    initialized Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnX3-oYMF3c_jc_6vcYho8lJtZJXhdvVw",
+  authDomain: "tune-lynk.firebaseapp.com",
+  projectId: "tune-lynk",
+  storageBucket: "tune-lynk.appspot.com",
+  messagingSenderId: "259611873330",
+  appId: "1:259611873330:web:d04b06a7e34f4d5b67ec2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
