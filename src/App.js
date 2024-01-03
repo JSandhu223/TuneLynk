4d83d081bdf79d564912d258005b27bafc18d086
JSandhu223 <jasrajsandhu01@gmail.com>
2024-01-02 23:46:27 -0700

    Repositioned search bar to appear in the middle of the navbar

--- src/App.js
+++ src/App.js
@@ -16,6 +16,12 @@ function App() {
           <li className="nav-item">
             <a className="nav-link" href="#">Library</a>
           </li>
+        </ul>
+        <form className="form">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <ul className="navbar-nav">
           <li className="nav-item">
             <a className="nav-link" href="#">Sign In</a>
           </li>
@@ -23,10 +29,7 @@ function App() {
             <a className="nav-link" href="#">Create Account</a>
           </li>
         </ul>
-        <form className="form">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+        
       </div>
     </nav>
   );
