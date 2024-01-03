e218f8bff7c8cfa61836282325d7e4d94a10b2f7
JSandhu223 <jasrajsandhu01@gmail.com>
2024-01-02 22:49:49 -0700

    Changed names of navbar links. Added search bar (form element)

--- src/App.js
+++ src/App.js
@@ -14,15 +14,19 @@ function App() {
             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
+            <a className="nav-link" href="#">Library</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="#">Sign In</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
+            <a className="nav-link" href="#">Create Account</a>
           </li>
         </ul>
+        <form className="form">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
       </div>
     </nav>
   );
