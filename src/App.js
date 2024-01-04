f4ec11f828ff3a362a0d741a4af2b34f395699df
JSandhu223 <jasrajsandhu01@gmail.com>
2024-01-03 21:36:18 -0700

    Page now properly displays all components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">TuneLynk</a>
+      <a className="navbar-brand" href="/#">TuneLynk</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="/#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Library</a>
+            <a className="nav-link" href="/#">Library</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Explore</a>
+            <a className="nav-link" href="/#">Explore</a>
           </li>
         </ul>
         <form className="form">
@@ -26,10 +25,10 @@ function Navbar() {
         </form>
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link" href="#">Sign In</a>
+            <a className="nav-link" href="/#">Sign In</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Create Account</a>
+            <a className="nav-link" href="/#">Create Account</a>
           </li>
         </ul>
       </div>
@@ -37,10 +36,21 @@ function Navbar() {
   );
 }
 
-function App() {
+function HomePage() {
   return (
-    <Navbar />
+    <div className="container">
+      <h1>Hello</h1>
+    </div>
   );
 }
 
-export default Navbar;
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+    </>
+  );
+}
+
+export { Navbar };
